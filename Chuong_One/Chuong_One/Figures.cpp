c12e02601f14702be6f6991e6564e75e68b0f3ad
Nguyen Trien <trienvaj@gmail.com>
2021-01-14 19:03:59 +0700

    class and object

---
+++ Chuong_One/Chuong_One/Figures.cpp
@@ -0,0 +1,55 @@
+﻿#include"Figures.h" // nhằm để biết cấu trúc của lớp Point2D, cicle
+#include <iostream>
+
+using namespace std;
+void Point2D::Set(double Xo, double Yo)
+{
+	X = Xo; 
+	Y = Yo; // Tương đương: this-> X = Xo; this-> Y = Yo;
+}
+void Point2D::Move(double dX, double dY)
+{
+	X += dX;
+	Y += dY;
+}
+void Point2D::Scale(double sX, double sY)
+{
+	X *= sX;
+	Y *= sY;
+}
+
+const double PI = 3.14159;
+
+void Circle::Set(double Xo, double Yo, double r)
+{
+	Center.Set(Xo, Yo);
+	if (r < 0)
+		r = 0;
+	this->Radius = r;	
+}
+void Circle::Move(double dX, double dY)
+{
+	Center.Move(dX, dY); //gọi phương thức của Point2D
+}
+double Circle::Area()
+{
+	return  PI * Radius * Radius;
+}
+double Circle::Perimeter()
+{
+	return 2 * PI * Radius;
+}
+void Circle::Set(double r)
+{
+	if (r >= 0)
+		Radius = r;
+}
+
+void main()
+{
+	Circle cir;
+	cir.Set(0, 10, 20);
+	cir.Set(-5); 
+	
+
+}
---
+++ Chuong_One/Chuong_One/Figures.h
@@ -0,0 +1,24 @@
+﻿
+#ifndef _FIGURE_H
+#define _FIGRE_H
+class Point2D
+{
+	double X, Y;
+public:
+	void Set(double Xo, double Yo);
+	void Move(double dX, double dY);
+	void Scale(double sX, double sY);
+
+};
+
+class Circle {
+	Point2D Center;
+	double Radius;
+public: 
+	void Set(double Xo, double Yo, double r);
+	void Set(double r);
+	void Move(double dX, double dY);
+	double Area();
+	double Perimeter();
+};
+#endif //2,3,23 hạn chế việc đọc đi đọc lại nhiều lần
